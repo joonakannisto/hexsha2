f17dbbe6702e8dd43ca77d3aca760274e78f25c4
Joona Kannisto <joona.kannisto@iki.fi>
2017-03-24 09:48:11 +0200

    Fix typos, assign arrays to slices

--- hexsha2.go
+++ hexsha2.go
@@ -7,20 +7,20 @@ import "encoding/base32"
 
 func HexDigest(input string) string {
 sum := sha256.Sum256([]byte(input))
-return hex.EncodeToString(sum)
+return hex.EncodeToString(sum[:])
 }
-func TruncHexDigest(len int, input string) {
+func TruncHexDigest(len int, input string) string {
  sum := sha256.Sum256([]byte(input)) 
- if (len > 64 || len<0) {
+ if (len > 64 || len<1) {
   len=64
 }
  return hex.EncodeToString(sum[:len/2])
 }
 func Base32Digest(input string) string {
 sum := sha256.Sum256([]byte(input)) 
-return base32.HexEncoding.EncodeToString(sum)
+return base32.HexEncoding.EncodeToString(sum[:])
 }
-func TruncBase32Digest(len int, input string) {
+func TruncBase32Digest(len int, input string) string{
  sum := sha256.Sum256([]byte(input)) 
  if (len > 52 || len<1) {
   len= 52
